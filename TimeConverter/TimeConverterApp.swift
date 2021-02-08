4e397d91e15bc34e81d973a7f44b724764d3a04b
prosvetovvv <vp12@yandex.ru>
2021-02-08 13:28:50 +0300

    Initial Commit

---
+++ TimeConverter/TimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverterApp.swift
+//  TimeConverter
+//
+//  Created by Vitaly Prosvetov on 08.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
