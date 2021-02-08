6370793c41e5026881b7a156b80297748831cc3d
prosvetovvv <vp12@yandex.ru>
2021-02-08 13:30:31 +0300

    Done

--- TimeConverter/ContentView.swift
+++ TimeConverter/ContentView.swift
@@ -7,15 +7,79 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+enum TimeUnits: String {
+    case seconds = "Seconds"
+    case minutes = "Minutes"
+    case hours = "Hours"
+    case days = "Days"
+    
+    var perSeconds: Double {
+        switch self {
+        case .seconds:
+            return 1
+        case .minutes:
+            return 1 * 60
+        case .hours:
+            return 60 * 60
+        case .days:
+            return 3600 * 24
+        }
     }
 }
-
+ 
+struct ContentView: View {
+    private let units: [TimeUnits] = [.seconds, .minutes, .hours, .days];
+    
+    @State private var inputValue = "";
+    @State private var inputUnit: TimeUnits = .seconds;
+    @State private var outputUnit: TimeUnits = .seconds;
+    
+    var inputValueBySeconds: Double {
+        let userValue = Double(inputValue) ?? 0
+        return userValue * inputUnit.perSeconds
+    }
+    
+    var outputValue: Double {
+        return inputValueBySeconds / outputUnit.perSeconds
+    }
+    
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("FROM")) {
+                    TextField("Enter value", text: $inputValue)
+                        .keyboardType(.numberPad)
+                    
+                    Picker("Select unit", selection: $inputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text("\($0.rawValue)")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("TO")) {
+                    Picker("Select unit", selection: $outputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text("\($0.rawValue)")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Result")) {
+                    Text("\(outputValue, specifier: "%.2f") \(outputUnit.rawValue.lowercased())")
+                }
+            }
+            .navigationBarTitle("Time Converter")
+        }
+    }
+}
+ 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
